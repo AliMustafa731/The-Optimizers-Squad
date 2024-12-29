a0df2013660f9fd7bdd261fff2407441d13ae419
Thoalfeqar <thoalfeqarkatajarullah@gmail.com>
2024-12-29 13:46:18 +0300

    This file will include utility function. For now, I have added a function that finds the optimal classification threshold based on the predictions of a model

---
+++ utilities.py
@@ -0,0 +1,23 @@
+from sklearn.metrics import roc_curve
+import pandas as pd, numpy as np
+
+#found from stack overflow
+def find_optimal_cutoff(target, predicted):
+    """ Find the optimal probability cutoff point for a classification model related to event rate
+    Parameters
+    ----------
+    target : Matrix with dependent or target data, where rows are observations
+
+    predicted : Matrix with predicted data, where rows are observations
+
+    Returns
+    -------     
+    list type, with optimal cutoff value
+        
+    """
+    fpr, tpr, threshold = roc_curve(target, predicted)
+    i = np.arange(len(tpr)) 
+    roc = pd.DataFrame({'tf' : pd.Series(tpr-(1-fpr), index=i), 'threshold' : pd.Series(threshold, index=i)})
+    roc_t = roc.iloc[(roc.tf-0).abs().argsort()[:1]]
+
+    return list(roc_t['threshold'])
