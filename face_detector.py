707f18333f8839fb39331bb6fc777956e0e6e82f
Thoalfeqar <thoalfeqarkatajarullah@gmail.com>
2024-12-31 18:06:02 +0300

    added a small comment

--- face_detector.py
+++ face_detector.py
@@ -2,7 +2,7 @@ import mediapipe as mp, os
 from mediapipe import tasks
 from collections import namedtuple
 from mediapipe.tasks.python import vision
-model_path = os.path.join('Data', 'Models', 'face detector', 'blaze_face_short_range.tflite')
+model_path = os.path.join('Data', 'Models', 'face detector', 'blaze_face_short_range.tflite') #path to the face detector
 
 def get_boundry_box(numpy_image , cropping_width=451 , cropping_hight=300):
     BaseOptions = tasks.BaseOptions
@@ -11,7 +11,7 @@ def get_boundry_box(numpy_image , cropping_width=451 , cropping_hight=300):
     VisionRunningMode = vision.RunningMode
     image_height , image_width , _ = numpy_image.shape
     x = (image_width  - cropping_width) // 2
-    y = (image_height  - cropping_hight) //2
+    y = (image_height  - cropping_hight) // 2
     # Create a face detector instance with the image mode:
     options = FaceDetectorOptions(
         base_options=BaseOptions(model_asset_path=model_path),
